1ebcf35427927a003c120144a120ce21672c3709
Kalle Wasnowicz <kai@wnsrt.de>
2024-03-27 18:26:29 +0100

    custom config.mk

--- config.mk
+++ config.mk
@@ -4,12 +4,15 @@ VERSION  = `git describe --tags --dirty 2>/dev/null || echo $(_VERSION)`
 PKG_CONFIG = pkg-config
 
 # paths
-PREFIX = /usr/local
+PREFIX = ~/.local
 MANDIR = $(PREFIX)/share/man
 DATADIR = $(PREFIX)/share
 
 XWAYLAND =
 XLIBS =
 # Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
+
+# Custom Flags
+CFLAGS = -march=native -O2 -pipe -flto=12 -Wl,-O1 -Wl,--as-needed -Wl,-z,pack-relative-relocs
