0ae68d3355e73f73113135933d271e431019c664
Kalle Wasnowicz <kai@wnsrt.de>
2024-03-27 22:54:31 +0100

    remove custom macbook settings

--- config.def.h
+++ config.def.h
@@ -64,8 +64,8 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5f,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	*/
 
-    /* Macbook*/
-    { "eDP-1",    0.55f,    1,      1.25,   &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
+    /* Macbook
+    { "eDP-1",    0.55f,    1,      1.25,   &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },*/
 	/* defaults */
 	{ NULL,       0.55f,    1,      1,      &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 };
