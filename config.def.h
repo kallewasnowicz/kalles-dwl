41f1c111e43d9e300aa282db70f509f6d7e83da4
kallewasnowicz <codeberg@wnsrt.de>
2024-03-31 12:31:17 +0200

    macbook custom resolution

--- config.def.h
+++ config.def.h
@@ -35,6 +35,7 @@ static const char *const autostart[] = {
     "swaync", NULL,
     "gentoo-pipewire-launcher", NULL,
     "foot", "-s", NULL,
+    "wlr-randr", "--output", "eDP-1", "--custom-mode", "'1920x1200@60'", NULL,
     NULL /* terminate */
 };
 
